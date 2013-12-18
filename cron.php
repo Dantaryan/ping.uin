1bc05036ba079bcf86d396157599a65bdbd93e5b
Dantaryan <evgpanchenko@gmail.com>
2013-12-18 21:58:57 +0200

    rev 0

--- cron.php
+++ cron.php
@@ -10,5 +10,6 @@ $o = new pinguin();
 $o->attachObserver(new streamPingNotify());
 $o->attachObserver(new jabberPingNotify());
 $o->attachObserver(new smsPingNotify());
+$o->attachObserver(new filelogPingNotify());
 
 $o->run();
